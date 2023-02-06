e33a6d119384d1302ef3d722a88190edaf60732d
SHALINI <drshaverma242@gmail.com>
2023-02-06 23:09:32 +0530

    init

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ reset.addEventListener('click', ()=>{
     });
     turn = "X"; 
     isgameover = false
-    document.querySelector(".line").style.width = "12vw";
+    document.querySelector(".line").style.width = "0vw";
     document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
